ebeed6ec5306db2214d226b2c4ded94227327299
Nikola <woleet@outlook.com>
2023-02-19 19:17:58 +0100

    API Photo upload

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -41,7 +41,7 @@ namespace API.Data
     {
       return await _context.Users
         .Include(p => p.Photos)
-        .SingleOrDefaultAsync(x => x.UserName == username);
+        .SingleOrDefaultAsync(u => u.UserName == username);
     }
 
     public async Task<IEnumerable<AppUser>> GetUsersAsync()
